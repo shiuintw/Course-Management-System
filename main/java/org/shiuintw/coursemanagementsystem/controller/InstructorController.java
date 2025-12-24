34fdc31e9e2f3a858a221f2c701057e09bef4c41
AjiHsu <mrhsu950325@gmail.com>
2025-12-24 12:18:38 +0800

    instructor info

--- main/java/org/shiuintw/coursemanagementsystem/controller/InstructorController.java
+++ main/java/org/shiuintw/coursemanagementsystem/controller/InstructorController.java
@@ -1,9 +1,13 @@
 package org.shiuintw.coursemanagementsystem.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.shiuintw.coursemanagementsystem.model.Building;
 import org.shiuintw.coursemanagementsystem.model.Instructor;
+import org.shiuintw.coursemanagementsystem.model.User;
 import org.shiuintw.coursemanagementsystem.service.InstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,9 +20,17 @@ public class InstructorController {
         this.instructorService = instructorService;
     }
 
-    @GetMapping("/{id}")
-    @ResponseBody
-    public Instructor getInstructor(@PathVariable String id) {
-        return instructorService.getInstructorById(id);
+    @GetMapping("instructor/{instructorId}")
+    public String getBuilding(@PathVariable String instructorId, Model model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+        }
+        if (instructorId == null || instructorId.isEmpty() || instructorId.equals("Unknown")) {
+            return "redirect:/course/search";
+        }
+        Instructor instructor = instructorService.getInstructorById(instructorId);
+        model.addAttribute("instructor", instructor);
+        return "instructor";
     }
 }
